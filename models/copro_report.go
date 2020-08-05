6250047a431a76905f9b94051d47a36369006ca4
Iledant <christophe.saintillan@gmail.com>
2020-08-05 17:00:54 +0200

    Refactor copro report

--- models/copro_report.go
+++ models/copro_report.go
@@ -28,32 +28,33 @@ type CoproReports struct {
 // GetAll fetches all lines of the copro report from database
 func (c *CoproReports) GetAll(db *sql.DB) error {
 	qry := `
-  WITH max_cmt_dat AS (SELECT max(creation_date) AS d FROM commitment)
+  WITH max_cmt_date AS (SELECT MAX(creation_date) AS d FROM commitment)
   SELECT ci.insee_code,ci.name,co.name,co.budget,cmt.value,prg.value,
     q.y1,q.y2,q.y3,q.y4,q.y5 FROM
-  (SELECT * FROM
-    crosstab('SELECT cf.id,EXTRACT(year FROM c.date),cf.value from copro_forecast cf
-      JOIN commission c ON cf.commission_id=c.id ORDER BY 1',
-      'select * FROM generate_series(extract(year FROM CURRENT_DATE)::bigint+1,
-      extract(year FROM CURRENT_DATE)::bigint+5)')
-    AS (id int, y1 BIGINT, y2 BIGINT, y3 BIGINT, y4 BIGINT, y5 BIGINT)) q
-  JOIN copro_forecast cf ON q.id=cf.id
-  JOIN copro co ON cf.copro_id=co.id
-  JOIN (SELECT sum(value)::bigint AS value,copro_id FROM commitment 
-    WHERE copro_id NOTNULL GROUP BY 2) cmt ON cmt.copro_id=co.id
-  LEFT OUTER JOIN (SELECT sum(value)::bigint AS value,kind_id AS copro_id FROM prog 
-    JOIN commission co ON prog.commission_id=co.id
-    WHERE kind=2 AND co.date>(SELECT d FROM max_cmt_dat) GROUP BY 2) prg 
-    ON prg.copro_id=co.id
-  JOIN city ci ON co.zip_code=ci.insee_code;`
+		(SELECT * FROM
+			crosstab(
+				'SELECT cf.id,EXTRACT(year FROM c.date),cf.value from copro_forecast cf
+					JOIN commission c ON cf.commission_id=c.id ORDER BY 1',
+				'SELECT * FROM generate_series(EXTRACT(year FROM CURRENT_DATE)::bigint+1,
+					EXTRACT(year FROM CURRENT_DATE)::bigint+5)')
+			AS (id int, y1 BIGINT, y2 BIGINT, y3 BIGINT, y4 BIGINT, y5 BIGINT)) q
+		JOIN copro_forecast cf ON q.id=cf.id
+		JOIN copro co ON cf.copro_id=co.id
+		JOIN (SELECT SUM(value)::bigint AS value,copro_id FROM commitment 
+			WHERE copro_id NOTNULL GROUP BY 2) cmt ON cmt.copro_id=co.id
+		LEFT OUTER JOIN (SELECT SUM(value)::bigint AS value,kind_id AS copro_id FROM prog 
+			JOIN commission co ON prog.commission_id=co.id
+			WHERE kind=2 AND co.date>(SELECT d FROM max_cmt_date) GROUP BY 2) prg 
+			ON prg.copro_id=co.id
+		JOIN city ci ON co.zip_code=ci.insee_code;`
 	rows, err := db.Query(qry)
 	if err != nil {
 		return fmt.Errorf("select %v", err)
 	}
 	var l CoproReport
 	for rows.Next() {
-		if err = rows.Scan(&l.InseeCode, &l.CityName, &l.CoproName, &l.Budget, &l.Commitment,
-			&l.Prog, &l.Y1, &l.Y2, &l.Y3, &l.Y4, &l.Y5); err != nil {
+		if err = rows.Scan(&l.InseeCode, &l.CityName, &l.CoproName, &l.Budget,
+			&l.Commitment, &l.Prog, &l.Y1, &l.Y2, &l.Y3, &l.Y4, &l.Y5); err != nil {
 			return fmt.Errorf("scan %v", err)
 		}
 		c.Lines = append(c.Lines, l)
