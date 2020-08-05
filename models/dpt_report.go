faf5d3b37655abd2fbe219a1975108da99e44e92
Iledant <christophe.saintillan@gmail.com>
2020-08-05 18:04:16 +0200

    Refactor dpt report

--- models/dpt_report.go
+++ models/dpt_report.go
@@ -23,53 +23,61 @@ type DptReport struct {
 // GetAll fetches the commitment and payment per department from database
 func (d *DptReport) GetAll(db *sql.DB, firstYear, lastYear int64) (err error) {
 	qry := fmt.Sprintf(`WITH
-	hcmt AS (SELECT d.code as dpt_code,cmt.year,SUM(cmt.value) as cmt
+	housingCmt AS (SELECT d.code as dpt_code,cmt.year,SUM(cmt.value) as cmt
 		FROM cumulated_commitment cmt
 		JOIN housing h ON cmt.housing_id=h.id
 		JOIN department d ON d.code=h.zip_code/1000
 		GROUP BY 1,2),
-  hpmt AS (SELECT d.code AS dpt_code,p.year,SUM(p.value) as pmt FROM payment p
+  housingPmt AS (SELECT d.code AS dpt_code,p.year,SUM(p.value) as pmt FROM payment p
 		JOIN cumulated_commitment c ON p.commitment_id=c.id
 		JOIN housing h ON c.housing_id=h.id
 		JOIN department d ON d.code=h.zip_code/1000
 		GROUP BY 1,2),
-  hfig AS (SELECT COALESCE(hcmt.dpt_code,hpmt.dpt_code) AS dpt_code,
-      COALESCE(hcmt.year,hpmt.year) AS year,hcmt.cmt,hpmt.pmt FROM hcmt
-  	FULL OUTER JOIN hpmt ON hcmt.dpt_code=hpmt.dpt_code AND hcmt.year=hpmt.year),
-	ccmt AS (SELECT d.code as dpt_code,cmt.year,SUM(cmt.value) as cmt 
+  housingFig AS (SELECT COALESCE(housingCmt.dpt_code,housingPmt.dpt_code) AS dpt_code,
+			COALESCE(housingCmt.year,housingPmt.year) AS year,housingCmt.cmt,housingPmt.pmt
+		FROM housingCmt
+		FULL OUTER JOIN housingPmt ON housingCmt.dpt_code=housingPmt.dpt_code 
+			AND housingCmt.year=housingPmt.year),
+	coproCmt AS (SELECT d.code as dpt_code,cmt.year,SUM(cmt.value) as cmt 
 		FROM cumulated_commitment cmt
 		JOIN copro co ON cmt.copro_id=co.id
 		JOIN department d ON d.code=co.zip_code/1000
 		GROUP BY 1,2),
-  cpmt AS (SELECT d.code AS dpt_code,p.year,SUM(p.value) as pmt FROM payment p
+  coproPmt AS (SELECT d.code AS dpt_code,p.year,SUM(p.value) as pmt FROM payment p
 		JOIN cumulated_commitment c ON p.commitment_id=c.id
 		JOIN copro co ON c.copro_id=co.id
 		JOIN department d ON d.code=co.zip_code/1000
 		GROUP BY 1,2),
-  cfig AS (SELECT COALESCE(ccmt.dpt_code,cpmt.dpt_code) AS dpt_code,
-      COALESCE(ccmt.year,cpmt.year) AS year,ccmt.cmt,cpmt.pmt FROM ccmt
-		FULL OUTER JOIN cpmt ON ccmt.dpt_code=cpmt.dpt_code AND ccmt.year=cpmt.year),
-	rcmt AS (SELECT d.code as dpt_code,cmt.year,SUM(cmt.value) as cmt 
+  coproFig AS (SELECT COALESCE(coproCmt.dpt_code,coproPmt.dpt_code) AS dpt_code,
+			COALESCE(coproCmt.year,coproPmt.year) AS year,coproCmt.cmt,coproPmt.pmt
+			FROM coproCmt
+		FULL OUTER JOIN coproPmt ON coproCmt.dpt_code=coproPmt.dpt_code 
+			AND coproCmt.year=coproPmt.year),
+	renewProjectCmt AS (SELECT d.code as dpt_code,cmt.year,SUM(cmt.value) as cmt 
 		FROM cumulated_commitment cmt
 		JOIN rp_cmt_city_join rp ON cmt.id=rp.commitment_id
 		JOIN department d ON d.code=rp.city_code/1000
 		GROUP BY 1,2),
-  rpmt AS (SELECT d.code AS dpt_code,p.year,SUM(p.value) as pmt FROM payment p
+	renewProjectPmt AS (SELECT d.code AS dpt_code,p.year,SUM(p.value) as pmt 
+		FROM payment p
 		JOIN cumulated_commitment c ON p.commitment_id=c.id
 		JOIN rp_cmt_city_join rp ON c.id=rp.commitment_id
 		JOIN department d ON d.code=rp.city_code/1000
 		GROUP BY 1,2),
-  rfig AS (SELECT COALESCE(rcmt.dpt_code,rpmt.dpt_code) AS dpt_code,
-    COALESCE(rcmt.year,rpmt.year) AS year,rcmt.cmt,rpmt.pmt FROM rcmt
-    FULL OUTER JOIN rpmt ON rpmt.dpt_code=rcmt.dpt_code AND rpmt.year=rcmt.year),
-  tfig AS (SELECT q.dpt_code,q.year,SUM(q.cmt) as cmt,SUM(q.pmt) as pmt FROM 
-    (SELECT * FROM cfig UNION ALL SELECT * FROM hfig UNION ALL SELECT * FROM rfig)q
+  renewProjectFig AS (SELECT COALESCE(rc.dpt_code,rp.dpt_code) dpt_code,
+		COALESCE(rc.year,rp.year) AS year,rc.cmt,rp.pmt
+		FROM renewProjectCmt rc
+		FULL OUTER JOIN renewProjectPmt rp ON rp.dpt_code=rc.dpt_code
+			AND rp.year=rc.year),
+  totalFig AS (SELECT q.dpt_code,q.year,SUM(q.cmt) as cmt,SUM(q.pmt) as pmt FROM 
+		(SELECT * FROM coproFig UNION ALL SELECT * FROM housingFig
+			UNION ALL SELECT * FROM renewProjectFig) q
     GROUP BY 1,2)
-	SELECT dpt.code,dpt.name,y.year,COALESCE(tfig.cmt,0)::bigint,
-			COALESCE(tfig.pmt,0)::bigint
+	SELECT dpt.code,dpt.name,y.year,COALESCE(totalFig.cmt,0)::bigint,
+			COALESCE(totalFig.pmt,0)::bigint
 		FROM department dpt
 		CROSS JOIN (SELECT generate_series(%d,%d) as year) y
-		LEFT OUTER JOIN tfig ON tfig.dpt_code=dpt.code AND tfig.year=y.year
+		LEFT OUTER JOIN totalFig ON totalFig.dpt_code=dpt.code AND totalFig.year=y.year
 		ORDER BY 1,2;`, firstYear, lastYear)
 	rows, err := db.Query(qry)
 	if err != nil {
